859fee110065ca04f26c4320de434144230464c1
hhejmanowski <hhejmanowski@gmail.com>
2026-09-01 04:52:48 -0500

    C: the triangle can move!

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state, log_event
+from player import *
 
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -13,7 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     #print(f"Screen width: {SCREEN_WIDTH}")
